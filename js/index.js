766b055947f05641c6d1eb1bdd1479f56670921f
Alexandre de Lemeny-Makedone <lemenyalex@protonmail.com>
2021-07-09 15:15:46 +0200

    ERROR with JS

--- js/index.js
+++ js/index.js
@@ -5,12 +5,16 @@ fetch ("http://localhost:3000/api/teddies/")
         }
     })
     .then(function(teddys) {
-        document.getElementById("teddy__0__name").innerText = teddys[0].name
-        document.getElementById("teddy__0__price").innerText = teddys[0].price + " €"   //TODO : Need conversion
-        document.getElementById("teddy__0__description").innerText = teddys[0].description
-        document.getElementById("teddy__0__description").innerHTML = "<img src='" + teddys[0].imageUrl + "' id =\"teddy__0__img\" class=\"img-fluid rounded\"  width=\"200\" height=\"250\" role=\"img\" aria-label=\"Ours en peluche fait main\" alt=\"Ours en peluche fait main\" focusable=\"false\"><title>Ours en peluche fait main</title></img> "
-
+        let i = 0;
+        console.log(teddys.length);
+        for(i in teddys.length) {
+            document.getElementById("teddy__" + i + "__name").innerText = teddys[i].name;
+            document.getElementById("teddy__" + i + "__price").innerText = teddys[i].price + " €";   //TODO : Need conversion
+            document.getElementById("teddy__" + i +"__description").innerText = teddys[i].description;
+            document.getElementById("teddy__" + i + "__img").innerHTML = "<img src='" + teddys[i].imageUrl + "' id =\"teddy__" + i + "__img\" class=\"img-fluid rounded\"  width=\"200\" height=\"250\" role=\"img\" aria-label=\"Ours en peluche fait main\" alt=\"Ours en peluche fait main\" >";
+        }
+        //TODO : Build the entire HTML part via JS
   })
-  .catch(function(err) {
-      console.log("Le site n'a pas pu récuperer les informations depuis le serveur. Avez-vous fait Node server ?");
+  .catch(function() {
+      console.error("The site could not retrieve the information from the server. Did you do \"Node server\"?");
   });
