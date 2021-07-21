568876bb8f96a35aa27d03ef9ab42951e415aa09
Nitatemic <13944652+nitatemic@users.noreply.github.com>
2021-07-21 15:03:38 +0200

    Finish file migration

--- js/index.js
+++ js/index.js
@@ -5,13 +5,16 @@ fetch ("http://localhost:3000/api/teddies/")
         }
     })
     .then(function(teddys) {
-        let i = 0;
         console.log(teddys.length);
-        for(i in teddys.length) {
+        console.log("coucou");  //TODO : delete
+        for(let i = 0; i < teddys.length; i++) {
+            console.log(i);
             document.getElementById("teddy__" + i + "__name").innerText = teddys[i].name;
             document.getElementById("teddy__" + i + "__price").innerText = teddys[i].price + " €";   //TODO : Need conversion
             document.getElementById("teddy__" + i +"__description").innerText = teddys[i].description;
             document.getElementById("teddy__" + i + "__img").innerHTML = "<img src='" + teddys[i].imageUrl + "' id =\"teddy__" + i + "__img\" class=\"img-fluid rounded\"  width=\"200\" height=\"250\" role=\"img\" aria-label=\"Ours en peluche fait main\" alt=\"Ours en peluche fait main\" >";
+            let link = document.getElementById("teddy__"+ i +"__link");
+            link.setAttribute("href", "./product/teddy" + i + ".html");
         }
         //TODO : Build the entire HTML part via JS
   })
