4e29a5f0c6c9b92d8f52a1b9e9b1a75c5214fa29
Alexandre de Lemeny-Makedone <alexandre@nitatemic.ovh>
2021-07-22 17:51:26 +0200

    Fix error on index

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ fetch ("http://localhost:3000/api/teddies/")
             console.log(i);
             document.getElementById("teddy__" + i + "__name").innerText = teddys[i].name;
             let temp = teddys[i].price / 100;
-            document.getElementById("teddy__" + i + "__price").innerText = teddys[i].price + " €";
+            document.getElementById("teddy__" + i + "__price").innerText = temp + " €";
             document.getElementById("teddy__" + i +"__description").innerText = teddys[i].description;
             document.getElementById("teddy__" + i + "__img").innerHTML = "<img src='" + teddys[i].imageUrl + "' id =\"teddy__" + i + "__img\" class=\"img-fluid rounded\"  width=\"200\" height=\"250\" role=\"img\" aria-label=\"Ours en peluche fait main\" alt=\"Ours en peluche fait main\" >";
             let link = document.getElementById("teddy__"+ i +"__link");
