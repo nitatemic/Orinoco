1ee5621d87f26f00559dd467f7a522bd3db595be
Nitatemic <13944652+nitatemic@users.noreply.github.com>
2021-07-22 17:43:45 +0200

    Price convertion

--- js/index.js
+++ js/index.js
@@ -6,11 +6,11 @@ fetch ("http://localhost:3000/api/teddies/")
     })
     .then(function(teddys) {
         console.log(teddys.length);
-        console.log("coucou");  //TODO : delete
         for(let i = 0; i < teddys.length; i++) {
             console.log(i);
             document.getElementById("teddy__" + i + "__name").innerText = teddys[i].name;
-            document.getElementById("teddy__" + i + "__price").innerText = teddys[i].price + " €";   //TODO : Need conversion
+            let temp = teddys[i].price / 100;
+            document.getElementById("teddy__" + i + "__price").innerText = teddys[i].price + " €";
             document.getElementById("teddy__" + i +"__description").innerText = teddys[i].description;
             document.getElementById("teddy__" + i + "__img").innerHTML = "<img src='" + teddys[i].imageUrl + "' id =\"teddy__" + i + "__img\" class=\"img-fluid rounded\"  width=\"200\" height=\"250\" role=\"img\" aria-label=\"Ours en peluche fait main\" alt=\"Ours en peluche fait main\" >";
             let link = document.getElementById("teddy__"+ i +"__link");
