2f35305b4e5dc636b2f705c76a5ab4ee70ad0871
Nitatemic <13944652+nitatemic@users.noreply.github.com>
2021-07-27 18:58:51 +0200

    Change method for get color

--- js/teddy0.js
+++ js/teddy0.js
@@ -12,13 +12,11 @@ fetch ("http://localhost:3000/api/teddies/")
         document.getElementById("teddy__" + i +"__description").innerText = teddys[i].description;
         document.getElementById("teddy__" + i + "__img").innerHTML = "<img src='" + teddys[i].imageUrl + "' id =\"teddy__" + i + "__img\" class=\"img-fluid rounded\"  width=\"400\" height=\"500\" role=\"img\" aria-label=\"Ours en peluche fait main\" alt=\"Ours en peluche fait main\" >";
 
+        //Faire une boucle qui creer une option pour chaque couleur
+        for (let j = 0; j < teddys[i].colors.length; j++) {
+            document.getElementById("colorschoise").innerHTML += "<option value=\" "+  teddys[i].colors[j] +"\" >" + teddys[i].colors[j] + "</option>";
+        }
 
-        for(let j = 0; j < teddys[i].colors.length; j++) {
-            document.getElementById("colorschoise").innerHTML = "<option value=\" "+  teddys[i].colors[0] +"\" >" + teddys[i].colors[0] + "</option>" +
-                "                            <option value=\" "+  teddys[i].colors[1] +"\" >" + teddys[i].colors[1] + "</option>" +
-                "                            <option value=\" "+  teddys[i].colors[2] +"\" >" + teddys[i].colors[2] + "</option>" +
-                "                            <option value=\" "+  teddys[i].colors[3] +"\" >" + teddys[i].colors[3] + "</option>";
-        }   //todo : Faire la boucle
     })
     .catch(function() {
         console.error("Oops, an error occurred. Did you do \"Node server\"?");
