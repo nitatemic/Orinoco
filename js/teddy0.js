6261db53b7705c11958ad664ccbcc2e43a9d4969
Alexandre de Lemeny-Makedone <alexandre@nitatemic.ovh>
2021-07-28 06:46:52 +0200

    API is now online! Img added with JS for Norbert

--- js/teddy0.js
+++ js/teddy0.js
@@ -1,19 +1,29 @@
-fetch("http://localhost:3000/api/teddies")
+fetch(apiUrl)
   .then(function (res) {
     if (res.ok) {
       return res.json();
     }
   })
   .then(function (teddys) {
-    //Faire une boucle qui creer une option pour chaque couleur
-    for (let j = 0; j < teddys[i].colors.length; j++) {
-      document.getElementById("colorschoise").innerHTML +=
-        '<option value=" ' +
-        teddys[i].colors[j] +
-        '" >' +
-        teddys[i].colors[j] +
-        "</option>";
-    }
+    //Créer une div dans la div container
+    var row1 = document.createElement("div");
+    row1.className = "row";
+    document.getElementById("container").appendChild(row1);
+    //Créer une div dans la div row
+    var col1 = document.createElement("div");
+    col1.className = "col";
+    row1.appendChild(col1);
+    //Créer une div dans la div col
+    var teddyImgContainer = document.createElement("div");
+    teddyImgContainer.className = "col-auto d-none d-lg-block";
+    col1.appendChild(teddyImgContainer);
+    //Ajouter l'image dans la div teddyImgContainer
+    var teddyImg = document.createElement("img");
+    teddyImg.src = apiUrl + "/../../images/teddy_1.jpg";
+    teddyImg.className = "img-fluid rounded";
+    teddyImg.width = "400";
+    teddyImg.height = "500";
+    teddyImgContainer.appendChild(teddyImg);
   })
   .catch(function () {
     console.error('Oops, an error occurred. Did you do "Node server"?');
