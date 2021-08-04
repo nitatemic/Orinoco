590a61fa428742b32f7eaaea5d404dda03b99ca0
Alexandre de Lemeny-Makedone <lemenyalex@protonmail.com>
2021-08-04 13:29:10 +0200

    Fix a logical error and delete html error alert

--- js/cart.js
+++ js/cart.js
@@ -47,8 +47,8 @@
             //Find to which teddys belongs the id passed in parameter and store its number in i
             for (let i = 0; i < products.length; i++) {
                 for(let j =0; i < teddys.length; j++) {
-                    if (products[j].id === teddys[j]._id) {
-                        console.log("coucou");
+                    if (products[i].id === teddys[j]._id) {
+                        console.log("coucou1");
                     }
                 }
             }
@@ -57,10 +57,5 @@
 
         .catch(function () {
             console.error("Oops, an error occurred. Please contact alexandre@nitatemic.ovh");
-            //Ajouter du html dans le container
-            let errorMsg = document.createElement("div");
-            errorMsg.className = "alert alert-danger";
-            errorMsg.role = "alert";
-            errorMsg.textContent = "Oops, an error occurred. Please contact alexandre@nitatemic.ovh";
-            document.getElementById("container").appendChild(errorMsg);
+
         });
