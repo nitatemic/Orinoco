5ce0f9e0815b67e5f1bf2ea7d33bc95a5d73ec92
Alexandre de Lemeny-Makedone <lemenyalex@protonmail.com>
2021-08-04 04:17:45 +0200

    convert var to let and add a submit

--- js/product.js
+++ js/product.js
@@ -8,7 +8,7 @@ fetch(apiUrl)
       const urlParams = new URLSearchParams(window.location.search);
       const id = urlParams.get("id");
       let i;
-      
+
       //Trouver à quel teddys appartient l'id passé en paramètre et stocké son numero dans i
       for (let j = 0; j < teddys.length; j++) {
         if (teddys[j]._id === id) {
@@ -18,22 +18,22 @@ fetch(apiUrl)
       }
 
     //Créer une div dans la div container
-    var row1 = document.createElement("div");
+    let row1 = document.createElement("div");
     row1.className = "row";
     document.getElementById("container").appendChild(row1);
 
     //Créer une div dans la div row
-    var col1 = document.createElement("div");
+    let col1 = document.createElement("div");
     col1.className = "col";
     row1.appendChild(col1);
 
     //Créer une div dans la div col
-    var teddyImgContainer = document.createElement("div");
+    let teddyImgContainer = document.createElement("div");
     teddyImgContainer.className = "col-auto d-none d-lg-block";
     col1.appendChild(teddyImgContainer);
 
     //Ajouter l'image dans la div teddyImgContainer
-    var teddyImg = document.createElement("img");
+    let teddyImg = document.createElement("img");
     teddyImg.src = teddys[i].imageUrl;
     teddyImg.className = "img-fluid rounded";
     teddyImg.width = "400";
@@ -41,48 +41,56 @@ fetch(apiUrl)
     teddyImgContainer.appendChild(teddyImg);
 
     //Créer un div dans la div container
-    var col2 = document.createElement("div");
+    let col2 = document.createElement("div");
     col2.className = "col mt-5";
     row1.appendChild(col2);
 
     //Créer un h1 dans la div col2
-    var h1 = document.createElement("h1");
+    let h1 = document.createElement("h1");
     h1.textContent = teddys[i].name;
     col2.appendChild(h1);
 
     //Créer un h2 dans la div col2
-    var h2 = document.createElement("h2");
+    let h2 = document.createElement("h2");
     h2.className = "h5";
     h2.textContent = teddys[i].description;
     col2.appendChild(h2);
 
     //Créer un h3 dans la div col2
-    var h3 = document.createElement("h3");
+    let h3 = document.createElement("h3");
     h3.textContent = teddys[i].price / 100 + "€";
     col2.appendChild(h3);
 
     //Créer un h4 dans la div container
-    var h4 = document.createElement("h4");
+    let h4 = document.createElement("h4");
     h4.className = "h6";
     h4.textContent = "Choissisez la couleur de votre teddy :";
     col2.appendChild(h4);
 
     //Créer un form dans la div col2
-    var form = document.createElement("form");
+    let form = document.createElement("form");
     form.className = "form-inline";
     col2.appendChild(form);
 
     //Créer un selection dans le form
-    var select = document.createElement("select");
+    let select = document.createElement("select");
     select.className = "dropdown";
     select.id = "colorschoise";
+
     //Ajouter toutes les couleurs dans le selection
     for (let j = 0; j < teddys[i].colors.length; j++)    {
-      var option = document.createElement("option");
+      let option = document.createElement("option");
       option.textContent = teddys[i].colors[j];
       select.appendChild(option);
     }
     form.appendChild(select);
+
+    //Ajouter un submit dans le form
+    let submit = document.createElement("button");
+    submit.type = "submit";
+    submit.className = "btn btn-warning";
+    submit.textContent = "Ajouter au panier";
+    form.appendChild(submit);
 
   })
   .catch(function () {
