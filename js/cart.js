3bc5c4e5d8073e4ae59e348b0166bb83732db86e
Nitatemic <13944652+nitatemic@users.noreply.github.com>
2021-08-05 16:41:03 +0200

    Bug added

--- js/cart.js
+++ js/cart.js
@@ -6,59 +6,59 @@ fetch(apiUrl)
     })
 
     .then(function (teddys) {
+      var products = [];
+      for (let i = 0; i < localStorage.length; i++) {
+        product = localStorage.key(i);
+        products.push(JSON.parse(localStorage.getItem(product)));
+      }
+      console.log(products); //Je recupere un tableau d'objets
 
-        var products = [];
-            for (let i = 0; i < localStorage.length; i++) {
-                product = localStorage.key(i);
-                products.push(JSON.parse(localStorage.getItem(product)));
-            }
-            console.log(products); //Je recupere un tableau d'objets
+      console.log(teddys[0]._id); //Je recupere un tableau d'objets)
 
-            console.log(teddys[0]._id); //Je recupere un tableau d'objets)
+      //Ajouter le nombre de produit dans la div numberOfItems
+      document.getElementById("numberOfItems").innerHTML = products.length;
 
-            //Ajouter le nombre de produit dans la div numberOfItems
-            document.getElementById("numberOfItems").innerHTML = products.length;
+      //Creer une list ul dans la div cart
+      let ul1 = document.createElement("ul");
+      ul1.id = ("cartListUl");
+      document.getElementById("cartList").appendChild(ul1);
 
-        //Find to which teddys belongs the id passed in parameter and store its number in i
-        for (let i = 0; i < products.length; i++) {
-            for(let j = 0; i < teddys.length; j++) {
-                if (products[i].id === teddys[j]._id) {
-                  console.log(teddys[i]._id); //Je recupere un tableau d'objets)
-                  console.log(teddys.length); //Je recupere un tableau d'objets)]
-                  console.log("coucou1");
-                }
-            }
+      //Find to which teddys belongs the id passed in parameter and store its number in i
+      for (let i = 0; i < products.length; i++) {
+        for (let j = 0; i < teddys.length - 1; j++) {
+          let teddy = teddys[j];
+          if (products[i].id === teddy._id) {
+              //Creer un element li dans la liste ul
+              let li = document.createElement("li");
+              //Ajouter le nom du produit dans le element li
+              li.innerHTML = products[i].name;
+              //Ajouter le prix du produit dans le element li
+              li.innerHTML += " - " + products[i].price;
+              //Ajouter la couleur du produit dans le element li
+              li.innerHTML += " - " + products[i].color;
+              document.getElementById("cartListUl").appendChild(li);
+          }
         }
+      }
 
-        if (products.length >= 0 === false) {
-            //Creer div dans la div cart
-            var emptyCartAlert = document.getElementById("div");
-            emptyCartAlert.className = "alert alert-danger";
-            emptyCartAlert.role = "alert";
-            document
-            .getElementById("CartList")
-            .appendChild(emptyCartAlert);
+      if (!(products.length >= 0)) {
+        //Creer div dans la div cart
+        let emptyCartAlert = document.getElementById("div");
+        emptyCartAlert.className = "alert alert-danger";
+        emptyCartAlert.role = "alert";
+        document.getElementById("CartList").appendChild(emptyCartAlert);
 
-            //Creer un h4 dans la div emptyCartAlert
-            var emptyCartAlertHeading = document.createElement("h4");
-            emptyCartAlertHeading.innerHTML = "Votre panier est vide";
-            emptyCartAlert.appendChild(emptyCartAlertHeading);
+        //Creer un h4 dans la div emptyCartAlert
+        let emptyCartAlertHeading = document.createElement("h4");
+        emptyCartAlertHeading.innerHTML = "Votre panier est vide";
+        emptyCartAlert.appendChild(emptyCartAlertHeading);
 
-            //Creer un p dans la div emptyCartAlert
-            var emptyCartAlertText = document.createElement("p");
-            emptyCartAlertText.innerHTML =
-            'Vous pouvez ajouter des produits dans votre panier en cliquant sur le bouton "Ajouter au panier"';
-            emptyCartAlert.appendChild(emptyCartAlertText);
-        } else {
-            //Creer une list ul dans la div cart
-            var cartList = document.getElementById("CartList");
-            for (var i = 0; i < products.length; i++) {
-            var product = products[i];
-            var productName = document.createElement("li");
-            productName.innerHTML = product.name;
-            cartList.appendChild(productName);
-            }
-        }
+        //Creer un p dans la div emptyCartAlert
+        let emptyCartAlertText = document.createElement("p");
+        emptyCartAlertText.innerHTML =
+          'Vous pouvez ajouter des produits dans votre panier en cliquant sur le bouton "Ajouter au panier"';
+        emptyCartAlert.appendChild(emptyCartAlertText);
+      }
     }
 )
 
