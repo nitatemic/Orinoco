ae9784d93129d6b6e3319861ce8a6898f5f7d29d
Nitatemic <13944652+nitatemic@users.noreply.github.com>
2021-08-10 21:19:03 +0200

    Cart total work

--- js/cart.js
+++ js/cart.js
@@ -42,8 +42,29 @@ fetch(apiUrl)
     for (var i = 0; i < cart.length; i++) {
       showCartItem(teddys[i].name, cart[i].color, teddys[i].price/100 + "€");
     }
-  }
-  )
+    createLi("totalLi" , "list-group-item d-flex justify-content-between", "cartListUl");
+
+    //Creer un span dans la li
+    let span3 = document.createElement("span");
+    span3.innerHTML = "Total (EUR)";
+    document.getElementById("totalLi").appendChild(span3);
+
+    //Creer un strong dans la li
+    let strong4 = document.createElement("strong");
+    strong4.innerHTML = totalCalc(cart);
+    document.getElementById("totalLi").appendChild(strong4);
+
+    function totalCalc(cart) {
+      let total = 0;
+      for (var i = 0; i < cart.length; i++) {
+        total += teddys[cart[i].i].price; //TODO : Trouver une autre méthode pour pouvoir la mettre en dehors du then
+      }
+      console.log(total);
+      return total /100 + "€";
+    }
+    })
+
+
 
   .catch(function () {
       console.error("Oops, an error occurred. Please contact alexandre@nitatemic.ovh");
@@ -90,4 +111,13 @@ function createItem(nameItem, colorItem, priceItem) {
   span2.className = "text-muted";
   span2.innerHTML = priceItem;
   document.getElementById(nameItem + "Li").appendChild(span2);
+}
+
+function totalCalc(cart) {
+  let total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    total += teddys[cart[i].i].price;
+  }
+  console.log(total);
+  return total;
 }
