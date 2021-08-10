995d4dcd8d17e4ac1c393e424347be4377d023bb
Nitatemic <13944652+nitatemic@users.noreply.github.com>
2021-08-10 18:49:29 +0200

    The cart is now an array before being stored in the localStorage

--- js/product.js
+++ js/product.js
@@ -100,11 +100,11 @@ fetch(apiUrl)
     submit.onclick = function (e) {
       e.preventDefault();
       const color = document.getElementById("colorschoise").value;
+      //TODO : Faire un test pour vérifier les données qui vont être envoyées
       getColor(id, color);
     };
     submit.textContent = "Ajouter au panier";
     form.appendChild(submit);
-
 
   })
 
@@ -118,18 +118,34 @@ fetch(apiUrl)
     document.getElementById("container").appendChild(errorMsg);
   });
 
-    //Créer une fonction qui recupere la couleur choisie par l'utilisateur dans le select ainsi que l'id du teddy et la stocke dans un objet avec localStorage
-    //Cette fonction sera appelée par le button "Ajouter au panier"
 
-    //Fonction qui recupere la couleur choisie par l'utilisateur dans le select ainsi que l'id du teddy et la stocke dans un string avec localStorage
-    function getColor(getColorId, getColorColor) {
-      const d = new Date();
+//Créer une fonction qui recupere la couleur choisie par l'utilisateur dans le select ainsi que l'id du teddy et la stocke dans un objet avec localStorage
+//Cette fonction sera appelée par le button "Ajouter au panier"
 
-      const temp = {
-        id: getColorId,
-        color: getColorColor,
-      };
+//Fonction qui recupere la couleur choisie par l'utilisateur dans le select ainsi que l'id du teddy et la stocke dans un string avec localStorage
+function getColor(getColorId, getColorColor) {
+  //const d = new Date();
 
-      //Ajouter l'objet avec les informations de l'utilisateur dans la variable localStorage
-      localStorage.setItem(d.getTime(), JSON.stringify(temp)); //J'utilise d.getTime pour générer un nom unique
-    }
+  const temp = {
+    id: getColorId,
+    color: getColorColor,
+  };
+
+  //Ajouter l'objet avec les informations de l'utilisateur dans la variable localStorage
+  //localStorage.setItem(d.getTime(), JSON.stringify(temp)); //J'utilise d.getTime pour générer un nom unique
+
+  //Si le localStorage ne contient pas de tableau appelée "cart", on l'initialise.
+  if (localStorage.getItem("cart") === null) {
+    localStorage.setItem("cart", JSON.stringify([temp]));
+  }
+
+  //On fait une copie temporaire du tableau qui est dans le localStorage
+  let tempCart = JSON.parse(localStorage.getItem("cart"));
+  //On ajoute l'objet avec les informations de l'utilisateur dans le tableau
+  tempCart.push(temp);
+  //On met à jour le localStorage avec le nouveau tableau
+  localStorage.setItem("cart", JSON.stringify(tempCart));
+
+
+
+}
