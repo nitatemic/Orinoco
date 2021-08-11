719eb0dc4588519b41c1a36bdedb51420ee06a6c
Alexandre de Lemeny-Makedone <lemenyalex@protonmail.com>
2021-08-11 14:39:21 +0200

    Multiple entry point config

---
+++ src/nn.js
@@ -0,0 +1,25 @@
+const apiUrl = "https://orinoco-nitatemic.herokuapp.com/api/teddies";
+import {
+  createUl,
+  showCartItem,
+  createLi,
+  createItem,
+  totalCalc,
+  showTeddies,
+} from "./functions.js";
+import "./sass/main.scss";
+
+fetch(apiUrl)
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (teddys) {
+    showTeddies(teddys);
+  })
+  .catch(function () {
+    console.error(
+      "Oops, an error occurred. Please contact alexandre@nitatemic.ovh"
+    );
+  });
