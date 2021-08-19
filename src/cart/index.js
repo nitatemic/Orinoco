353ce5b3d43e610ce5618bd6b3d6172a3332a795
Alexandre de Lemeny-Makedone <lemenyalex@protonmail.com>
2021-08-19 14:45:15 +0200

    Cart in checkout page work (exept total)

--- src/cart/index.js
+++ src/cart/index.js
@@ -7,13 +7,17 @@ import {
   showNumberOfItems,
 } from "./functions.js";
 
-    //Je récupere le tableau "cart" stocké dans le localStorage
-    let cart = JSON.parse(localStorage.getItem("cart"));
+//Je récupere le tableau "cart" stocké dans le localStorage
+let cart = JSON.parse(localStorage.getItem("cart"));
 
-    //Ajouter le nombre de produit dans la div numberOfItems
-    showNumberOfItems(cart, "numberOfItems");
+//Ajouter le nombre de produit dans la div numberOfItems
+showNumberOfItems(cart, "numberOfItems");
 
-fetch(apiUrl)
+createUl("cartListUl", "list-group mb-3", "cartList");
+
+for (let i = 0; i < cart.length; i++) {
+  let id = cart[i].id;
+  fetch(apiUrl + "/" + id)
   .then(function (res) {
     if (res.ok) {
       return res.json();
@@ -21,40 +25,34 @@ fetch(apiUrl)
   })
 
   .then(function (teddys) {
-
-
-
-      createUl("cartListUl", "list-group mb-3", "cartList");
-      //Faire une boucle qui parcours le tableau "cart"
-      for (let i = 0; i < cart.length; i++) {
-        showCartItem(
-          teddys[cart[i].i].name,
-          cart[i].color,
-          teddys[cart[i].i].price / 100 + "€",
-          cart.indexOf(cart[i])
-        );
-      }
-      createLi(
-        "totalLi",
-        "list-group-item d-flex justify-content-between",
-        "cartListUl"
-      );
-
-      //Creer un span dans la li
-      let span3 = document.createElement("span");
-      span3.innerHTML = "Total (EUR)";
-      document.getElementById("totalLi").appendChild(span3);
-
-      //Creer un strong dans la li
-      let strong4 = document.createElement("strong");
-      strong4.innerHTML = totalCalc(cart, teddys);
-      document.getElementById("totalLi").appendChild(strong4);
-    }
-  })
+    showCartItem(
+      teddys.name,
+      cart[i].color,
+      teddys.price / 100 + "€",
+      cart.indexOf(cart[i])
+    );
+})
 
   .catch(function () {
     console.error(
       "Oops, an error occurred. Please contact alexandre@nitatemic.ovh"
     );
   });
+}
+
+ createLi(
+   "totalLi",
+   "list-group-item d-flex justify-content-between",
+   "cartListUl"
+ );
+
+ //Creer un span dans la li
+ let span3 = document.createElement("span");
+ span3.innerHTML = "Total (EUR)";
+ document.getElementById("totalLi").appendChild(span3);
+
+ //Creer un strong dans la li
+ let strong4 = document.createElement("strong");
+ strong4.innerHTML = totalCalc(cart);
+ document.getElementById("totalLi").appendChild(strong4);
 
