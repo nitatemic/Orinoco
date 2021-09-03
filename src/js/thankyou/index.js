7e031601c027623542df937fc65e3fa2b171ed1c
Nitatemic <13944652+nitatemic@users.noreply.github.com>
2021-09-03 17:17:38 +0200

    Add DOMPurify

--- src/js/thankyou/index.js
+++ src/js/thankyou/index.js
@@ -1,16 +1,28 @@
 import {totalOrder, escapeHTML} from "./functions.js";
+import {sanitize} from "dompurify/dist/purify.min.js";
 
 const orderDetail = JSON.parse(sessionStorage.getItem("order"));
 
 //Modifier le h1 de la page de remerciment
-document.querySelector("h1").innerHTML = "Merci pour votre commande " + escapeHTML(orderDetail.contact.firstName) + " !";
+document.querySelector("h1").innerHTML =
+  "Merci pour votre commande " +
+  sanitize(orderDetail.contact.firstName) +
+  " !";
 
 
 //Modifier le p de la page de remerciment
 const orderNumber = document.getElementById("orderNumber");
-orderNumber.innerHTML = "Vous avez la commande n° " + "<strong>" + escapeHTML(orderDetail.orderId) + "</strong> !";
+orderNumber.innerHTML =
+  "Vous avez la commande n° " +
+  "<strong>" +
+  sanitize(orderDetail.orderId) +
+  "</strong> !";
 
 //Creer un p dans yourOrder
 const orderTotal = document.createElement("p");
-orderTotal.innerHTML = "Pour rappel votre commande a un montant total de " + "<strong>" + escapeHTML(totalOrder(orderDetail.products)) + "</strong>.";
+orderTotal.innerHTML =
+  "Pour rappel votre commande a un montant total de " +
+  "<strong>" +
+  sanitize(totalOrder(orderDetail.products)) +
+  "</strong>.";
  document.getElementById("yourOrder").appendChild(orderTotal);
