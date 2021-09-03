58f1d60b08a26cc797267fd88f5eff4bc53e5cff
Alexandre de Lemeny-Makedone <lemenyalex@protonmail.com>
2021-09-03 16:43:37 +0200

    Add function escapeHTML

--- src/js/thankyou/index.js
+++ src/js/thankyou/index.js
@@ -1,16 +1,16 @@
-import {totalOrder} from "./functions.js";
+import {totalOrder, escapeHTML} from "./functions.js";
 
 const orderDetail = JSON.parse(sessionStorage.getItem("order"));
 
 //Modifier le h1 de la page de remerciment
-document.querySelector("h1").innerHTML = "Merci pour votre commande " + orderDetail.contact.firstName + " !";
+document.querySelector("h1").innerHTML = "Merci pour votre commande " + escapeHTML(orderDetail.contact.firstName) + " !";
 
 
 //Modifier le p de la page de remerciment
 const orderNumber = document.getElementById("orderNumber");
-orderNumber.innerHTML = "Vous avez la commande n° " + "<strong>" + orderDetail.orderId + "</strong> !";
+orderNumber.innerHTML = "Vous avez la commande n° " + "<strong>" + escapeHTML(orderDetail.orderId) + "</strong> !";
 
 //Creer un p dans yourOrder
 const orderTotal = document.createElement("p");
-orderTotal.innerHTML = "Pour rappel votre commande a un montant total de " + "<strong>" + totalOrder(orderDetail.products) + "</strong>.";
+orderTotal.innerHTML = "Pour rappel votre commande a un montant total de " + "<strong>" + escapeHTML(totalOrder(orderDetail.products)) + "</strong>.";
  document.getElementById("yourOrder").appendChild(orderTotal);
