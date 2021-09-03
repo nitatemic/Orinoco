680b4d3ffb1d36b828b991ae6e7d8580e511eae0
Nitatemic <13944652+nitatemic@users.noreply.github.com>
2021-09-03 16:43:37 +0200

    Add function escapeHTML

--- src/js/thankyou/functions.js
+++ src/js/thankyou/functions.js
@@ -1,4 +1,4 @@
-/* ------------ Fonction qui fait le total de la commande ----------*/
+/* ------------ Fonction qui fait le total de la commande ---------- */
 function totalOrder(cart)   {
     let total = 0;
     for (var i = 0; i < cart.length; i++) {
@@ -6,5 +6,17 @@ function totalOrder(cart)   {
     }
     return total/ 100 + "€";
 }
-/* ----------------- Fonction qui fait le total de la commande --------------*/
-export { totalOrder };
+/* ---------- Fonction qui fait le total de la commande ---------- */
+
+/* ---------- Fonction qui protege (Vite fait) des XSS ---------- */
+function escapeHTML(toOutput) {
+  return toOutput
+    .replace(/\&/g, "&amp;")
+    .replace(/\</g, "&lt;")
+    .replace(/\>/g, "&gt;")
+    .replace(/\"/g, "&quot;")
+    .replace(/\'/g, "&#x27")
+    .replace(/\//g, "&#x2F");
+}
+/* ---------- Fin fonction qui protege (Vite fait) des XSS ---------- */
+export { totalOrder, escapeHTML };
