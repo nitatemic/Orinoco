307e3838a416bd8dbd444f06a3c66541f365175d
Nitatemic <13944652+nitatemic@users.noreply.github.com>
2021-09-22 00:47:12 +0200

    Fix error on thank you page

--- src/js/thankyou/index.js
+++ src/js/thankyou/index.js
@@ -4,7 +4,7 @@ import {sanitize} from "dompurify/dist/purify.min.js";
 const orderDetail = JSON.parse(sessionStorage.getItem("order"));
 
 //Créer le h1 de la page de remerciement
-createHType("h1", "Merci pour votre commande " + sanitize(orderDetail.contact.firstName) + " !" , "h1Container")
+createHType("h1", "Merci pour votre commande " + sanitize(orderDetail.contact.firstName) + " !" , "yourOrder")
 
 //Modifier le p de la page de remerciement
 createHType("p", "Vous avez la commande n° " + sanitize(orderDetail.orderId) + " !" , "yourOrder")
